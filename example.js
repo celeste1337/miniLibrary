729c9c430ade24e2d8f8748318422071f423b3c5
Celeste Yost <17mcyost@gmail.com>
2021-05-05 15:24:03 -0400

    more examples that are slightly more complicated

--- example.js
+++ example.js
@@ -1,9 +1,12 @@
 //
-//
 // this will be a file that shows how this library COULD be used
 //
 
 import { mini } from './library.js';
+
+//
+// Mini.Array.Map Functionality
+//
 
 //
 // example 1
@@ -22,15 +25,30 @@ let sampleArrayOfNames = ["Celeste&#","Owen/!&","Hobbes%","Maggie$"];
 //     return str
 // }
 
-//but this sucks! its so much code!!!!!!!!!
-//so now we do it like this instead which is less code and way more reusable~
+//but this sucks! its so much code!!!!!!!!! and such inefficient code too!
+//so now we do it like this instead which is less code and way more reusable
+//you feed it the array and the function you want it to do
 let newArray = mini.array.map(sampleArrayOfNames)
     (name => name.replace(/[^a-zA-Z0-9 ]/g, ""))
 console.log(newArray)
 
-//this one just returns the elements lol
+//this one just returns the elements
 let newArrayAgain = mini.array.map(sampleArrayOfNames)(name => name)
 console.log(newArrayAgain)
 
-//you can see that our data is still whole and unchanged :)
-console.log(sampleArrayOfNames)
+//
+// example 2 - the purpose of this example is to show how flexible this arrangement is. i can feed it numbers instead of strings and still have it do this cool wacky stuff!
+//
+
+let sampleArrayOfNumbers = [1,2,3,4,5];
+let numberArray = mini.array.map(sampleArrayOfNumbers)(number => number += 1)
+console.log(numberArray)
+
+
+//
+// example 3 - now with objects >:)
+//
+let sampleArrayOfObjects = [{name: "celeste", hp: 100},{name:"owen", hp: 100},{name: "hobbes", hp:100},{name: "maggie", hp: 100}];
+let objectArray = mini.array.map(sampleArrayOfObjects)(character => character.hp -= 10)
+console.log(objectArray);
+
--- library.js
+++ library.js
@@ -5,10 +5,16 @@
 //
 
 const mini = {
+    pipe: (func1) => (func2) => {
+        return func2(func1())
+    },
     array: {
         map: (arr) => (f) => {
-            return arr.map(f)
+            return arr.map(f);
         },
+        filter: (arr) => (m) => {
+            return arr.filter(m);
+        }
     },
     string: {},
     debug: {},
